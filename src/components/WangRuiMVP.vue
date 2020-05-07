effebf0ab4b6ff286c0d4d38fd349f1dc03a95b9
wangrui <1257983649@qq.com>
2020-05-07 13:48:23 +0800

    qq

---
+++ src/components/WangRuiMVP.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+	export default {
+	  name: 'WangRui',
+	  props: {
+	    msg: String
+	  }
+	}
+</script>
+
+<style>
+</style>
---
+++ src/views/Info.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<button id="btn" @click="name">提交</button>
+		<div id="anketo">{{project}}</div>
+	</div>
+</template>
+
+<script>
+	import HelloWorld from '@/components/HelloWorld.vue'
+	export default {
+		data(){
+			return{
+				project:{},
+				anketoList:[]
+			}
+		},
+		name: 'Info',
+		components: {
+			HelloWorld
+		},
+		methods: {
+			name:function() {
+				this.$axios.get('http://127.0.0.1:8080/whole/getbyid/1').then((res)=>{
+					this.project = res.data.Project
+					console.log('res:',res.data.Project)
+				}).catch(err =>{
+					console.log(err)
+				}).finally(() =>{
+					
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
